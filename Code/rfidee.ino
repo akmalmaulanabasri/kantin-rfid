39a91b1fb27f691ec38b8bf2bf93a7ad9243b5aa
Akmal Maulana Basri <editorzone.20@gmail.com>
2023-09-06 21:28:09 +0700

    upd

--- Code/rfidee.ino
+++ Code/rfidee.ino
@@ -1,18 +1,23 @@
 #include <LiquidCrystal_I2C.h>
 
+
 #include <SPI.h>
 #include <MFRC522.h>
 
+
 #include <ESP8266WiFi.h>
 #include <FirebaseESP8266.h>
+
 
 // Informasi jaringan WiFi
 const char* ssid = "XYZ";
 const char* password = "12345678";
 
+
 // Informasi Firebase
 #define FIREBASE_HOST "https://rfidkantin-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "AIzaSyDBBB875QO88NHqWHomJC6MHFm7l4bi_ak"
+
 
 FirebaseData firebaseData;
   
@@ -21,14 +26,19 @@ FirebaseData firebaseData;
 #define SS_PIN D8
 #define RST_PIN D0
 
+
 MFRC522 rfid(SS_PIN, RST_PIN);
 
+
 MFRC522::MIFARE_Key key;
+
 
 // Inisialisasi memulai mengirim NUID
 byte nuidPICC[4];
 
+
 LiquidCrystal_I2C lcd(0x27,16,2);
+
 
 void setup() {
  
@@ -63,34 +73,42 @@ void setup() {
   Serial.println("Alamat IP: ");
   Serial.println(WiFi.localIP());
 
+
   // Menghubungkan ke Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Serial.println("Firebase terhubung");
   SPI.begin(); // Memulai komunikasi SPI
   rfid.PCD_Init(); // Init MFRC522
 
+
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
+
 
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
   Serial.print(F("Using the following key:"));
   printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 }
 
+
 void loop() {
+
 
   // Cek untuk kartu baru
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
 
+
   // Verifikasi apakah NUID sudah dibaca
   if ( ! rfid.PICC_ReadCardSerial())
     return;
 
+
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   Serial.println(rfid.PICC_GetTypeName(piccType));
+
 
   // Check is the PICC of Classic MIFARE type
   if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
@@ -100,11 +118,13 @@ void loop() {
     return;
   }
 
+
   if (rfid.uid.uidByte[0] != nuidPICC[0] ||
     rfid.uid.uidByte[1] != nuidPICC[1] ||
     rfid.uid.uidByte[2] != nuidPICC[2] ||
     rfid.uid.uidByte[3] != nuidPICC[3] ) {
     Serial.println(F("A new card has been detected."));
+
 
     // Store NUID into nuidPICC array
     for (byte i = 0; i < 4; i++) {
@@ -133,15 +153,19 @@ void loop() {
   }
   else Serial.println(F("Card read previously."));
 
+
   // Halt PICC
   rfid.PICC_HaltA();
+
 
   // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
 
 
 
+
 }
+
 
 
 /**
@@ -154,6 +178,7 @@ void printHex(byte *buffer, byte bufferSize) {
   }
 }
 
+
 /**
  * Sub routine mengirim nilai DEX.
  */
@@ -163,3 +188,4 @@ void printDec(byte *buffer, byte bufferSize) {
     Serial.print(buffer[i], DEC);
   }
 }
+
